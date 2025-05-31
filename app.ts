a7bf43c8e354be8a4142ccefec88623323eb5998
frannpineroo <frannpinero@hotmail.com>
2025-05-30 23:16:34 -0300

    probando nodejs con la terminal de la computadora

--- app.ts
+++ app.ts
@@ -1,4 +1,14 @@
+// strings 
+let firstName: string = 'Fernando';
+let lastName: string = 'Herrera';
 
-console.log("Hola Mundo!");
+// console.log('Hola', firstName, lastName );
+let fullName: string = `${ firstName } ${ lastName }`;
 
+console.log('Hola', fullName );
+
+let num1: number = 10;
+let num2: number = 20;
+
+console.log(num1 + num2);
 
