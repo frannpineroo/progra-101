4dc723755b9ca9e0c933204632fd2f544f84e9ae
frannpineroo <frannpinero@hotmail.com>
2025-05-30 22:24:32 -0300

    pruebas con los files de typescript

---
+++ sections/section-2.ts
@@ -0,0 +1,13 @@
+let numberOfLines = 0;
+
+function printLineNum() {
+    console.log('Linea #', numberOfLines);
+    numberOfLines += 2;
+}
+
+printLineNum();
+printLineNum();
+printLineNum();
+printLineNum();
+
+
