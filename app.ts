0943c5a93a908f01e360d28ea5829da205e00783
frannpineroo <frannpinero@hotmail.com>
2025-06-02 20:43:01 -0300

    aprendiendo a crear tipos de datos en typescript

--- app.ts
+++ app.ts
@@ -1,14 +1,4 @@
-// strings 
-let firstName: string = 'Fernando';
-let lastName: string = 'Herrera';
 
-// console.log('Hola', firstName, lastName );
-let fullName: string = `${ firstName } ${ lastName }`;
 
-console.log('Hola', fullName );
-
-let num1: number = 10;
-let num2: number = 20;
-
-console.log(num1 + num2);
+console.log('Hola mundo')
 
---
+++ sections/basic-types.ts
@@ -0,0 +1,24 @@
+// strings 
+export let firstName: string = 'Fernando';
+let lastName: string = 'Herrera';
+
+// console.log('Hola', firstName, lastName );
+let fullName: string = `${ firstName } ${ lastName }`;
+
+console.log('Hola', fullName );
+
+let num1: number = 10;
+let num2: number = 20;
+
+console.log(num1 + num2);
+
+let isActive: boolean = true;
+let isRunning: boolean = false;
+let hasKids: boolean = true;
+
+isActive = !isActive;
+
+console.log('isActive', isActive);
+console.log('isRunning', isRunning);
+console.log('hasKids', hasKids);
+
