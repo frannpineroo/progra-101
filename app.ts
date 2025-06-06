8cca0694aacdd452b33e2b5aaf9783ecfafc4a2a
frannpineroo <frannpinero@hotmail.com>
2025-06-06 17:56:30 -0300

    aprendiendo ciclo for con ts

--- app.ts
+++ app.ts
@@ -1,10 +1,2 @@
 
-let gasTank:number = 0;
-
-do {
-    console.log("Gasolina restante:", gasTank);
-
-    gasTank -= 10; // Consuming 10 units of gas in each iteration
-} while (gasTank > 0);
-
-console.log("Ya no tiene gasolina")
+console.log("Hello, World!");
---
+++ sections/for.ts
@@ -0,0 +1,13 @@
+
+for (let i = 0; i <= 10; i++) {
+    if (i === 5) {
+        continue
+    }
+
+    console.log("i:", i)
+
+    if (i === 5) {
+        break
+    }
+}
+console.log("Fin del programa")
