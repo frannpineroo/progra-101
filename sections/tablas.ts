73dbf5ce73b1abc3598ee21277592bd84cbef99f
frannpineroo <frannpinero@hotmail.com>
2025-06-06 18:26:41 -0300

    poniendo a prueba mis aprendizajes haciendo la tabla del 10 con ciclo while

---
+++ sections/tablas.ts
@@ -0,0 +1,8 @@
+export let base:number = 1;
+let limit:number = 50;
+
+
+while (base <= limit) {
+    console.log("10 x", base, "=", base * 10);
+    base++; // imprime la tabla del 10 desde el 1 hasta el 50
+}
